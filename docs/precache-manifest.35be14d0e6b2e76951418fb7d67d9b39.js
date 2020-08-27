0e3c99af5510d7412f1feff76e6a56c2f3007602
Luchimen <luchimen@gmail.com>
2020-08-27 11:30:33 -0500

    docs agregados

---
+++ docs/precache-manifest.35be14d0e6b2e76951418fb7d67d9b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6f8aa6411e4229a6014f1f5a99dc3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01233101a4aeb2ae49c",
+    "url": "/static/css/main.0dd30b38.chunk.css"
+  },
+  {
+    "revision": "fb516b340910ba393099",
+    "url": "/static/js/2.3eab1664.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3eab1664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01233101a4aeb2ae49c",
+    "url": "/static/js/main.f89fa7fa.chunk.js"
+  },
+  {
+    "revision": "eb3b510adea8c34c3d1e",
+    "url": "/static/js/runtime-main.a25cd96d.js"
+  }
+]);
